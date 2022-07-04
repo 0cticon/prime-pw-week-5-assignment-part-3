edab6b2686756909c5b22a8e82b1b1f25cdfa8bb
Kyle VanTassell <octicon@gmail.com>
2022-07-04 12:13:26 -0500

    more albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,7 +13,7 @@ function addToCollection(title, artist, yearPublished) {
 addToCollection('Portrait of a Legend', 'Sam Cooke', 2003);
 addToCollection('Bitte Orca', 'Dirty Projectors', 2009);
 addToCollection('In Rainbows', 'Radiohead', 2007);
-addToCollection()
-addToCollection()
-addToCollection()
+addToCollection('The Soft Bulletin', 'The Flaming Lips', 1999);
+addToCollection('This is Happening', 'LCD Soundsystem', 2010);
+addToCollection('Hurry up, We\'re dreaming.', 'M83.', 2011);
 console.table(collection);
