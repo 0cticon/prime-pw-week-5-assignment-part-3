74981b4fa6514deaa8276e345e412a343966526c
Kyle VanTassell <octicon@gmail.com>
2022-07-04 12:04:11 -0500

    adding albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
 console.log('***** Music Collection *****')
+const collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    const newAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    };
+    collection.push(newAlbum);
+    return newAlbum;
+}
+addToCollection('Portrait of a Legend', 'Sam Cooke', 2003);
+addToCollection('Bitte Orca', 'Dirty Projectors', 2009);
+addToCollection('In Rainbows', 'Radiohead', 2007);
+addToCollection()
+addToCollection()
+addToCollection()
+console.table(collection);
