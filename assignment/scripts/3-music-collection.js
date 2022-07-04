035cd7cc9db06bd29646ba3224a6f0a3bccf1e01
Kyle VanTassell <octicon@gmail.com>
2022-07-04 12:40:04 -0500

    anotherAlbum

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,10 +10,13 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(newAlbum);
     return newAlbum;
 }
-addToCollection('Portrait of a Legend', 'Sam Cooke', 2003);
-addToCollection('Bitte Orca', 'Dirty Projectors', 2009);
-addToCollection('In Rainbows', 'Radiohead', 2007);
-addToCollection('The Soft Bulletin', 'The Flaming Lips', 1999);
-addToCollection('This is Happening', 'LCD Soundsystem', 2010);
-addToCollection('Hurry up, We\'re dreaming.', 'M83.', 2011);
+console.log(addToCollection('Portrait of a Legend', 'Sam Cooke', 2003));
+console.log(addToCollection('Bitte Orca', 'Dirty Projectors', 2009));
+console.log(addToCollection('In Rainbows', 'Radiohead', 2007));
+console.log(addToCollection('The Soft Bulletin', 'The Flaming Lips', 1999));
+console.log(addToCollection('This is Happening', 'LCD Soundsystem', 2010));
+console.log(addToCollection('Hurry up, We\'re dreaming.', 'M83.', 2011));
+console.log(addToCollection('Yoshimi Battles the Pink Robots', 'The Flaming Lips', 2002));
+
 console.table(collection);
+
